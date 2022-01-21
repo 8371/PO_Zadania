fbbcd51ed4d2325cfc0835fc10295dbc6b0bef7c
Krystian Kręcik <56454269+Krystiankr@users.noreply.github.com>
2022-01-21 05:27:21 +0100

    Pierwsza część, wszystkie zadania od 1 do 6.

--- MaratonPO/pracownik.cs
+++ MaratonPO/pracownik.cs
@@ -16,16 +16,23 @@ namespace MaratonPO
         // także jest wirtualna.
         public virtual void Pracuj()
         {
-            Console.WriteLine("Pracownik pracuje");
+            Console.WriteLine("logowanie");
         }
     }
     class Programista: pracownik
     {
-        //2. Stwórz klasę potomną Programista i użyj przesłaniania metody
+        // 2. Stwórz klasę potomną Programista i użyj przesłaniania metody
         //(zaimplementuj metodę pracuj). 
+        // 5. Nie podkreśla się metoda pracuj, świadomie nadpisujemy
+        // metodę Pracuj z klasy rodzica
+
+        // 6. Zmień metodę Pracuj w klasie pracownik na wirtualną, a następnie zamień wyświetlane dane na „logowanie”. W klasie Programista zmień kod metody Pracuj na następujący:
+        // base.Pracuj();
+        // Console.WriteLine("Obowiazki programisty");
         public override void Pracuj()
         {
-            Console.WriteLine("Przyslaniam metode pracuj");
+            base.Pracuj();
+            Console.WriteLine("Obowiazki programisty");
         }
     }
 }
