9f7ae7788011f8fbcaf3b10b7765dbc07bebe860
Krystian Kręcik <56454269+Krystiankr@users.noreply.github.com>
2022-01-21 05:08:52 +0100

    Add project files.

---
+++ MaratonPO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MaratonPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            pracownik p = new pracownik();
+            p.Pracuj();
+            Programista p1 = new Programista();
+            p1.Pracuj();
+        }
+    }
+}
---
+++ MaratonPO/pracownik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaratonPO
+{
+    class pracownik
+    {
+        // Zad 1
+        public void Pracuj()
+        {
+            Console.WriteLine("Pracownik pracuje");
+        }
+    }
+    class Programista: pracownik
+    {
+        public void Pracuj()
+        {
+            Console.WriteLine("Przyslaniam metode pracuj");
+        }
+    }
+}
